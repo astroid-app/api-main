1a654f995192c4fff073a52c0bed11da7b70be59
Deutscher <81298350+Deutscher775@users.noreply.github.com>
2022-12-07 20:18:39 +0100

    Adding API (includes: API; Bot) WITHOUT config file

---
+++ init.py
@@ -0,0 +1,37 @@
+import traceback
+import nextcord
+import json
+from nextcord.ext import commands
+import config
+import asyncio
+import os
+
+client = commands.Bot(command_prefix=".", intents=nextcord.Intents.all())
+
+
+@client.event
+async def on_ready():
+    for filename in os.listdir('cogs'):
+        if filename.endswith('.py'):
+            try:
+                client.load_extension(f'cogs.{filename[:-3]}')
+            except:
+                traceback.print_exc()
+    client.loop.create_task(sync_commands())
+    client.loop.create_task(change_presence())
+
+
+async def sync_commands():
+    while True:
+        await client.sync_application_commands()
+        await asyncio.sleep(1)
+
+
+async def change_presence():
+    await client.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.watching, name="Your Guilded Server"))
+    await asyncio.sleep(30)
+    await client.change_presence(activity=nextcord.Activity(type=nextcord.ActivityType.watching, name="Guildcord API"))
+    await asyncio.sleep(30)
+
+
+client.run(config.TOKEN)
